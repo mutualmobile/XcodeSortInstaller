ccfa1313684dd5a469f44bda78e40665913a249f
Lars Anderson <lars.anderson@mutualmobile.com>
2013-07-18 20:12:25 -0500

    Modulated a bunch of stuff out

--- lib/xcode_install_sort.rb
+++ lib/xcode_install_sort.rb
@@ -4,13 +4,22 @@ require 'fileutils'
 
 module XcodeInstallSort
   class XcodeSortInstaller
-    def sort_script_file_path
-      file_name = "sort-Xcode-project-file.pl"
-          t = ["#{File.dirname(File.expand_path($0))}/../lib/xcode_install_sort/#{file_name}",
-               "#{Gem.dir}/gems/xcode_install_sort-#{XcodeInstallSort::VERSION}/lib/xcode_install_sort/#{file_name}"]
-          t.each {|i| return i if File.readable?(i) }
-          raise "both paths are invalid: #{t}"
+    def install_sort_on_project(project_file_location, verbose)
+      puts "Integrating sort script into targets in #{project_file_location}"
+
+      proj = Xcodeproj::Project.new(project_file_location)
+
+      proj.objects.each do |object|
+        #What happens when a project has a sub-project?
+        if object.class == Xcodeproj::Project::Object::PBXProject
+          if process_project_object?(object, verbose)
+            save_project(object, project_file_location, verbose)
+            #check for .gitignore file
+            #append sort file timestamp file to .gitignore
+          end
         end
+      end
+    end
     
     def install_targets_from_project(project_object, verbose)
 
@@ -31,49 +40,51 @@ module XcodeInstallSort
       return script_targets
     end
     
-    def install_sort_on_project(project_file_location, verbose)
-      puts "Integrating sort script into targets in #{project_file_location}"
+    def process_project_object?(project, verbose)
+      first_target = project.targets[0]
 
-      proj = Xcodeproj::Project.new(project_file_location)
+      script_targets = install_targets_from_project(project, verbose)
 
-      proj.objects.each do |object|
-
-        #What happens when a project has a sub-project?
-        if object.class == Xcodeproj::Project::Object::PBXProject
-          first_target = object.targets[0]
-
-          script_targets = install_targets_from_project(object, verbose)
-          
-          puts "Current directory: #{Dir.pwd}"
-
-          if (script_targets.count > 0)
-            base_folder = File.dirname(project_file_location)
-            puts "Copying sort script to project location: #{base_folder}"
-            FileUtils.cp sort_script_file_path, "#{base_folder}/sort-Xcode-project-file.pl", :verbose => verbose
-          end
-
-          script_text = File.open("#{Dir.pwd}/sort-phase.sh").read
-
-          script_targets.each do |target|
-            puts "Adding sort script to #{target} target" if verbose
-
-            uuid = object.project.generate_uuid
-
-            script = target.new_shell_script_build_phase("Sort Project File")
-            script.shell_script = script_text
-          end
-
-          puts "Saving project file modifications..." if verbose
-
-          if object.project.save_as(project_file_location)
-            puts "Successfully integrated sort phase to #{script_targets.count.to_s} #{"target".pluralize(script_targets.count)}.\n"
-            puts "The next time you build your project, your project file will sort itself. This *will* result in massive project-file changes. Coordinate with others on your team to ensure their versions of the project file are also sorted. It is advisable you commit these project file changes and new scripts into source control before making further changes."
-          else
-            puts "Error saving project file."
-          end
-        end
+      script_targets.each do |target|
+        add_sort_script_to_target(target, verbose)
       end
+      
+      puts "Successfully integrated sort phase to #{script_targets.count.to_s} #{"target".pluralize(script_targets.count)}.\n"
+      
+      return (script_targets.count > 0)
     end
     
+    def add_sort_script_to_target(target, verbose)
+      puts "Adding sort script to #{target} target" if verbose
+
+      uuid = target.project.generate_uuid
+
+      target_shell_script_text_path = included_file_path("sort-phase.sh")
+      script_text = File.open(target_shell_script_text_path).read
+      
+      script = target.new_shell_script_build_phase("Sort Project File")
+      script.shell_script = script_text
+    end
+    
+    def included_file_path(file_name)
+      t = ["#{File.dirname(File.expand_path($0))}/../lib/xcode_install_sort/#{file_name}",
+        "#{Gem.dir}/gems/xcode_install_sort-#{XcodeInstallSort::VERSION}/lib/xcode_install_sort/#{file_name}"]
+        t.each {|i| return i if File.readable?(i) }
+      raise "both paths are invalid: #{t}"
+    end
+    
+    def save_project(project, project_file_location, verbose)
+      puts "Saving project file modifications..." if verbose
+      
+      base_folder = File.dirname(project_file_location)
+      puts "Copying sort script to project location: #{base_folder}"
+      FileUtils.cp included_file_path("sort-Xcode-project-file.pl"), "#{base_folder}/sort-Xcode-project-file.pl", :verbose => verbose
+
+      if project.project.save_as(project_file_location)
+        puts "The next time you build your project, your project file will sort itself. This *will* result in massive project-file changes. Coordinate with others on your team to ensure their versions of the project file are also sorted. It is advisable you commit these project file changes and new scripts into source control before making further changes."
+      else
+        puts "Error saving project file."
+      end
+    end
   end
 end
